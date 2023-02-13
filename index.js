c8b05cf6e9db3256e72cac140ecfdaf5ee59e990
raiane-oliveira <raiane.oliveira404@gmail.com>
2023-02-13 17:29:32 -0300

    :sparkles: feat: botão selecionado

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const thankYouState = document.querySelector(".thank-you-state")
 const ratingState = document.querySelector(".rating-state")
+const ratingButtons = document.querySelectorAll(".btn-rating")
+const submitBtn = document.querySelector(".btn-submit")
+let starRating = 0;
 
 // Esconde o thankYouState por padrão
 thankYouState.style.display = "none"
+
+document.addEventListener("click", (event) => {
+    const element = event.target
+
+    ratingButtons.forEach((button) => {
+        if (button.className.includes("btn-rating-clicked")) {
+            button.classList.remove("btn-rating-clicked")
+            button.classList.add("hover")
+        }
+    })
+            
+    element.classList.add("btn-rating-clicked")
+    element.classList.remove("hover")
+
+    // Armazena a classificação
+    starRating = element.dataset.rating
+})
