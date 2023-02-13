179b89b78c1f8829672c3d34a5a715ee9e4fdcd4
raiane-oliveira <raiane.oliveira404@gmail.com>
2023-02-13 16:30:40 -0300

    :sparkles: style: background e esconde o conteúdo

---
+++ index.js
@@ -0,0 +1,5 @@
+const thankYouState = document.querySelector(".thank-you-state")
+const ratingState = document.querySelector(".rating-state")
+
+// Esconde o thankYouState por padrão
+thankYouState.style.display = "none"
